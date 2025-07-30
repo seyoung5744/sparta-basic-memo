6d7da0f6b8d89ecfbc8c49a7c3f8aefff44b3871
seyoung5744 <seyoung574458@gmail.com>
2025-07-30 19:45:51 +0900

    create memo proejct

---
+++ src/main/java/com/example/basicmemo/BasicMemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicmemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicMemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicMemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/basicmemo/BasicMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicmemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicMemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
